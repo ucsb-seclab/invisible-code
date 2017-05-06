99cff8199cc54a966fe71f3027f30959bc98f401
ocean <ocean.kuzuri@gmail.com>
2017-05-06 13:53:29 -0700

    add helloianni to deps (allows compiling it automatically)

--- optee_qemu/build/common.mk
+++ optee_qemu/build/common.mk
@@ -337,7 +337,7 @@ update_rootfs-clean-common:
 	rm -f $(GEN_ROOTFS_FILELIST)
 
 filelist-tee-common: fl:=$(GEN_ROOTFS_FILELIST)
-filelist-tee-common: optee-client xtest helloworld helloblob
+filelist-tee-common: optee-client xtest helloworld helloblob helloianni
 	@echo "# filelist-tee-common /start" 				> $(fl)
 	@echo "dir /lib/optee_armtz 755 0 0" 				>> $(fl)
 	@echo "# xtest / optee_test" 					>> $(fl)
