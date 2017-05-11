fc1a40b760669947786bbbeb68c4d44549941032
Michele Ianni <michele.ianni@yahoo.it>
2017-05-11 11:48:57 -0700

    benchmark

---
+++ optee_qemu/benchmark/Android.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Android benchmark makefile                                           #
+################################################################################
+LOCAL_PATH := $(call my-dir)
+
+CFG_TEEC_PUBLIC_INCLUDE = $(LOCAL_PATH)/../optee_client/public
+
+################################################################################
+# Build benchmark                                                              #
+################################################################################
+include $(CLEAR_VARS)
+LOCAL_CFLAGS += -DANDROID_BUILD
+LOCAL_CFLAGS += -Wall
+
+LOCAL_SRC_FILES += host/benchmark.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/ta/include \
+		$(CFG_TEEC_PUBLIC_INCLUDE) \
+
+LOCAL_SHARED_LIBRARIES := libteec
+LOCAL_MODULE := tee_benchmark
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
